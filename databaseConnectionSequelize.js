4b088804b1b78c450fd523e80d3ff132add01512
Octavio Villena <octaviovillena@hotmail.com>
2021-03-29 18:44:55 -0700

    corrected dbConfigHeroku to have the right creds

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,6 @@
 const is_heroku = process.env.IS_HEROKU || false;
 const dbConfigHeroku =
-  "mysql://b1ab7fb2ee03bc:2a484a2d@us-cdbr-east03.cleardb.com/heroku_3d208ad4bd6f421";
+  "mysql://be92f2823cdbe0:562283e6@us-cdbr-east03.cleardb.com/heroku_f3df4ecbfcf7594";
 const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
 if (is_heroku) {
   var databaseConnectionString = dbConfigHeroku;
