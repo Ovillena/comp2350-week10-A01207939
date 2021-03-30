7d5a63de1085280e862848ae86a8999a75225733
Octavio Villena <octaviovillena@hotmail.com>
2021-03-29 18:48:07 -0700

    minor change to config string

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,6 +1,6 @@
 const is_heroku = process.env.IS_HEROKU || false;
 const dbConfigHeroku =
-  "mysql://be92f2823cdbe0:562283e6@us-cdbr-east03.cleardb.com/heroku_f3df4ecbfcf7594";
+  "mysql://be92f2823cdbe0:562283e6@us-cdbr-east-03.cleardb.com/heroku_f3df4ecbfcf7594";
 const dbConfigLocal = "mysql://root:Password@localhost/lab_example";
 if (is_heroku) {
   var databaseConnectionString = dbConfigHeroku;
@@ -8,3 +8,5 @@ if (is_heroku) {
   var databaseConnectionString = dbConfigLocal;
 }
 module.exports = databaseConnectionString;
+
+mysql://be92f2823cdbe0:562283e6@us-cdbr-east-03.cleardb.com/heroku_f3df4ecbfcf7594?reconnect=true
